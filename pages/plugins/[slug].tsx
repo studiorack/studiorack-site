2d9b958337cebe5e03aa255f575f733c323ac7dd
Kim T <kimturley@gmail.com>
2020-07-22 21:53:20 -0700

    Add play/pause audio button

--- pages/plugins/[slug].tsx
+++ pages/plugins/[slug].tsx
@@ -1,32 +1,82 @@
+import { Component } from 'react'
 import Layout from '../../components/layout'
 import { getAllPluginPaths, getPluginData, Plugin } from '../../lib/plugins'
 import Head from 'next/head'
 import styles from '../../styles/plugin.module.css'
 import { GetStaticProps, GetStaticPaths } from 'next'
-import { useRouter } from 'next/router'
+import { withRouter, Router } from 'next/router'
 
-export default function Post({
-  plugin
-}: {
+class Post extends Component<{
+  plugin: Plugin,
+  router: Router
+}, {
+  isPlaying: Boolean,
+  router: Router,
   plugin: Plugin
-}) {
-  const {basePath} = useRouter()
-  return (
+}> {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isPlaying: false,
+      plugin: props.plugin,
+      router: props.router
+    }
+  }
+
+  play = () => {
+    const el = document.getElementById('audio') as HTMLAudioElement
+    el.addEventListener('ended', (e) => {
+      this.setState({
+        isPlaying: false
+      });
+    });
+    if (el.paused) {
+      el.currentTime = 0;
+      el.play();
+    }
+    this.setState({
+      isPlaying: true
+    });
+  }
+
+  pause = () => {
+    const el = document.getElementById('audio') as HTMLAudioElement
+    if (!el.paused) {
+      el.pause();
+      this.setState({
+        isPlaying: false
+      });
+    }
+  }
+
+  render() {
+    return (
     <Layout>
       <Head>
-        <title>{plugin.name}</title>
+        <title>{this.state.plugin.name}</title>
       </Head>
       <article>
         <div className={styles.header}>
           <div className={styles.headerInner}>
-            <img className={styles.image} src={`https://github.com/${plugin.id}/releases/latest/download/plugin.png`} alt={plugin.name} />
+            <div className={styles.media}>
+              <div className={styles.imageContainer}>
+              {this.state.isPlaying ?
+                <img className={styles.imagePlay} src={`${this.state.router.basePath}/images/icon-pause.svg`} alt="Pause" onClick={this.pause} />
+                :
+                <img className={styles.imagePlay} src={`${this.state.router.basePath}/images/icon-play.svg`} alt="Play" onClick={this.play} />
+              }
+                <img className={styles.image} src={`https://github.com/${this.state.plugin.id}/releases/latest/download/plugin.png`} alt={this.state.plugin.name} />
+              </div>
+              <audio src={`https://github.com/${this.state.plugin.id}/releases/latest/download/plugin.wav`} id="audio">Your browser does not support the audio element.</audio>
+            </div>
             <div className={styles.details}>
-              <h3 className={styles.title}>{plugin.name} <span className={styles.version}>v{plugin.version}</span></h3>
-              <p className={styles.author}>By <a href={plugin.homepage} target="_blank">{plugin.author}</a></p>
-              <p>{plugin.description}</p>
+              <h3 className={styles.title}>{this.state.plugin.name} <span className={styles.version}>v{this.state.plugin.version}</span></h3>
+              <p className={styles.author}>By <a href={this.state.plugin.homepage} target="_blank">{this.state.plugin.author}</a></p>
+              <p>{this.state.plugin.description}</p>
               <ul className={styles.tags}>
-                <img className={styles.icon} src={`${basePath}/images/icon-tag.svg`} alt="Tags" />
-                {plugin.tags.map((tag) => (
+                <img className={styles.icon} src={`${this.state.router.basePath}/images/icon-tag.svg`} alt="Tags" />
+                {this.state.plugin.tags.map((tag) => (
                   <li className={styles.tag} key={tag}>{tag},</li>
                 ))}
               </ul>
@@ -37,20 +87,22 @@ export default function Post({
           <div className={styles.row}>
             <div className={`${styles.cell} ${styles.download}`}>
               <p>Download .zip file:</p>
-              <a className={`button ${styles.button}`} href={`https://github.com/${plugin.id}/releases/latest/download/plugin-linux.zip`}>Linux</a>
-              <a className={`button ${styles.button}`} href={`https://github.com/${plugin.id}/releases/latest/download/plugin-mac.zip`}>MacOS</a>
-              <a className={`button ${styles.button}`} href={`https://github.com/${plugin.id}/releases/latest/download/plugin-win.zip`}>Windows</a>
+              <a className={`button ${styles.button}`} href={`https://github.com/${this.state.plugin.id}/releases/latest/download/plugin-linux.zip`}>Linux</a>
+              <a className={`button ${styles.button}`} href={`https://github.com/${this.state.plugin.id}/releases/latest/download/plugin-mac.zip`}>MacOS</a>
+              <a className={`button ${styles.button}`} href={`https://github.com/${this.state.plugin.id}/releases/latest/download/plugin-win.zip`}>Windows</a>
             </div>
             <div className={`${styles.cell} ${styles.install}`}>
               <p>Install via command line:</p>
-              <pre className={styles.codeBox}>studiorack install {plugin.id}</pre>
+              <pre className={styles.codeBox}>studiorack install {this.state.plugin.id}</pre>
             </div>
           </div>
         </div>
       </article>
     </Layout>
-  )
+    )
+  }
 }
+export default withRouter(Post)
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPluginPaths()
