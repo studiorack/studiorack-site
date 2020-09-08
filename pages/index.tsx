d9f4568c4d8846e12e42cca0af8bda0f6a09876b
Kim T <kimturley@gmail.com>
2020-09-07 17:48:47 -0700

    Open source banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,13 +28,13 @@ class Home extends Component<HomeProps, {
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={`${styles.section} ${styles.sectionCreators}`}>
+        <section className={`${styles.section} ${styles.sectionOpenSource}`}>
           <div className={styles.container}>
             <div className={styles.inner}>
-              <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/creators-mobile.jpg`} alt="Creators" />
-              <h2 className={styles.title}>Automate your plugin publishing workflow</h2>
-              <p>Our plugin starter template uses GitHub Actions to build your plugin with the Steinberg VST3 SDK, publishing a new version straight to Github Releases.</p>
-              <a className="button" href={`${this.state.router.basePath}/docs`}>Get started</a>
+              <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/studio-audio-rack-mobile.jpg`} alt="Open source" />
+              <h2 className={styles.title}>An open-source audio plugin ecosystem</h2>
+              <p>Our tools are built using GitHub and open-source libraries, ensuring you will always be able to access them.</p>
+              <a className="button" href={`${this.state.router.basePath}/plugins`}>Browse plugins</a>
             </div>
           </div>
         </section>
@@ -44,6 +44,16 @@ class Home extends Component<HomeProps, {
               <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/producers-mobile.jpg`} alt="Producers" />
               <h2 className={styles.title}>Easy plugin installation &amp; management</h2>
               <p>Our app and command line tools allow you to search our plugin registry for free plugins. You can install and manage plugin versions all from one place.</p>
+              <a className="button" href={`${this.state.router.basePath}/docs`}>Get started</a>
+            </div>
+          </div>
+        </section>
+        <section className={`${styles.section} ${styles.sectionCreators}`}>
+          <div className={styles.container}>
+            <div className={styles.inner}>
+              <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/creators-mobile.jpg`} alt="Creators" />
+              <h2 className={styles.title}>Automate your plugin publishing workflow</h2>
+              <p>Our plugin starter template uses GitHub Actions to build your plugin with the Steinberg VST3 SDK, publishing a new version straight to Github Releases.</p>
               <a className="button" href={`${this.state.router.basePath}/docs`}>Get started</a>
             </div>
           </div>
