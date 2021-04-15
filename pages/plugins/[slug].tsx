2a2eb7edbc827cd5ed2a77cc5b1f4103139d2050
Kim T <kimturley@gmail.com>
2021-04-14 23:09:46 -0700

    Lint code

--- pages/plugins/[slug].tsx
+++ pages/plugins/[slug].tsx
@@ -1,200 +1,258 @@
-import { Component } from 'react'
-import Layout from '../../components/layout'
-import Head from 'next/head'
-import styles from '../../styles/plugin.module.css'
-import { GetStaticPaths } from 'next'
-import { withRouter, Router } from 'next/router'
-import { PluginInterface, pluginGet, pluginsGet } from '@studiorack/core'
-import { idToSlug, slugToId } from '../../node_modules/@studiorack/core/dist/utils'
+import { Component } from 'react';
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import styles from '../../styles/plugin.module.css';
+import { GetStaticPaths } from 'next';
+import { withRouter, Router } from 'next/router';
+import { PluginInterface, pluginGet, pluginsGet } from '@studiorack/core';
+import { idToSlug, slugToId } from '../../node_modules/@studiorack/core/dist/utils';
 
 type PluginProps = {
-  plugin: PluginInterface,
-  router: Router
-}
+  plugin: PluginInterface;
+  router: Router;
+};
 
-class PluginPage extends Component<PluginProps, {
-  isPlaying: boolean,
-  router: Router,
-  plugin: PluginInterface
-}> {
-
+class PluginPage extends Component<
+  PluginProps,
+  {
+    isPlaying: boolean;
+    router: Router;
+    plugin: PluginInterface;
+  }
+> {
   constructor(props: PluginProps) {
-    super(props)
+    super(props);
     this.state = {
       isPlaying: false,
       plugin: props.plugin,
-      router: props.router
-    }
+      router: props.router,
+    };
   }
 
-  formatBytes(bytes:number, decimals = 2) {
-    if (bytes === 0) return '0 Bytes'
-    const k = 1024
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+  formatBytes(bytes: number, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 
-  timeSince(date:string) {
-    const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000)
-    let interval = seconds / 31536000
+  timeSince(date: string) {
+    const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
+    let interval = seconds / 31536000;
     if (interval > 1) {
-      return Math.floor(interval) + " years"
+      return Math.floor(interval) + ' years';
     }
-    interval = seconds / 2592000
+    interval = seconds / 2592000;
     if (interval > 1) {
-      return Math.floor(interval) + " months"
+      return Math.floor(interval) + ' months';
     }
-    interval = seconds / 86400
+    interval = seconds / 86400;
     if (interval > 1) {
-      return Math.floor(interval) + " days"
+      return Math.floor(interval) + ' days';
     }
-    interval = seconds / 3600
+    interval = seconds / 3600;
     if (interval > 1) {
-      return Math.floor(interval) + " hours"
+      return Math.floor(interval) + ' hours';
     }
-    interval = seconds / 60
+    interval = seconds / 60;
     if (interval > 1) {
-      return Math.floor(interval) + " minutes"
+      return Math.floor(interval) + ' minutes';
     }
-    return Math.floor(seconds) + " seconds"
+    return Math.floor(seconds) + ' seconds';
   }
 
   play = () => {
-    const el = document.getElementById('audio') as HTMLAudioElement
+    const el = document.getElementById('audio') as HTMLAudioElement;
     if (el.paused) {
-      el.removeEventListener('ended', this.ended)
-      el.addEventListener('ended', this.ended)
-      el.currentTime = 0
-      el.play()
-      this.setState({ isPlaying: true })
+      el.removeEventListener('ended', this.ended);
+      el.addEventListener('ended', this.ended);
+      el.currentTime = 0;
+      el.play();
+      this.setState({ isPlaying: true });
     }
-  }
+  };
 
   pause = () => {
-    const el = document.getElementById('audio') as HTMLAudioElement
+    const el = document.getElementById('audio') as HTMLAudioElement;
     if (!el.paused) {
-      el.pause()
-      this.setState({ isPlaying: false })
+      el.pause();
+      this.setState({ isPlaying: false });
     }
-  }
+  };
 
   ended = () => {
-    this.setState({ isPlaying: false })
-  }
+    this.setState({ isPlaying: false });
+  };
 
   getPlayButton() {
     if (this.state.isPlaying) {
-      return <img className={styles.imagePlay} src={`${this.state.router.basePath}/images/icon-pause.svg`} alt="Pause" onClick={this.pause} />
+      return (
+        <img
+          className={styles.imagePlay}
+          src={`${this.state.router.basePath}/images/icon-pause.svg`}
+          alt="Pause"
+          onClick={this.pause}
+        />
+      );
     } else {
-      return <img className={styles.imagePlay} src={`${this.state.router.basePath}/images/icon-play.svg`} alt="Play" onClick={this.play} />
+      return (
+        <img
+          className={styles.imagePlay}
+          src={`${this.state.router.basePath}/images/icon-play.svg`}
+          alt="Play"
+          onClick={this.play}
+        />
+      );
     }
   }
 
   render() {
     return (
-    <Layout>
-      <Head>
-        <title>{this.state.plugin.name || ''}</title>
-      </Head>
-      <article>
-        <div className={styles.header}>
-          <div className={styles.headerInner}>
-            <div className={styles.media}>
-              <div className={styles.imageContainer}>
-              {this.state.plugin.files.audio ?
-                this.getPlayButton()
-                : ''
-              }
-              {this.state.plugin.files.image ?
-                <img className={styles.image} src={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.image.name}`} alt={this.state.plugin.name || ''} />
-                : ''
-              }
+      <Layout>
+        <Head>
+          <title>{this.state.plugin.name || ''}</title>
+        </Head>
+        <article>
+          <div className={styles.header}>
+            <div className={styles.headerInner}>
+              <div className={styles.media}>
+                <div className={styles.imageContainer}>
+                  {this.state.plugin.files.audio ? this.getPlayButton() : ''}
+                  {this.state.plugin.files.image ? (
+                    <img
+                      className={styles.image}
+                      src={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.image.name}`}
+                      alt={this.state.plugin.name || ''}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+                {this.state.plugin.files.audio ? (
+                  <audio
+                    src={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.audio.name}`}
+                    id="audio"
+                  >
+                    Your browser does not support the audio element.
+                  </audio>
+                ) : (
+                  ''
+                )}
               </div>
-              {this.state.plugin.files.audio ?
-                <audio src={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.audio.name}`} id="audio">Your browser does not support the audio element.</audio>
-                : ''
-              }
-            </div>
-            <div className={styles.details}>
-              <h3 className={styles.title}>{this.state.plugin.name || ''} <span className={styles.version}>v{this.state.plugin.version}</span></h3>
-              <p className={styles.author}>By <a href={this.state.plugin.homepage} target="_blank">{this.state.plugin.author}</a></p>
-              <p>{this.state.plugin.description}</p>
-              <div className={styles.metadataList}>
-                {/* <div className={styles.metadata}><img className={styles.icon} src={`${this.state.router.basePath}/images/icon-filesize.svg`} alt="Filesize" /> {this.formatBytes(this.state.plugin.size)}</div> */}
-                <div className={styles.metadata}><img className={styles.icon} src={`${this.state.router.basePath}/images/icon-date.svg`} alt="Date updated" /> {this.timeSince(this.state.plugin.date)} ago</div>
-                <div className={styles.metadata}>
-                  <img className={styles.icon} src={`${this.state.router.basePath}/images/icon-tag.svg`} alt="Tags" />
+              <div className={styles.details}>
+                <h3 className={styles.title}>
+                  {this.state.plugin.name || ''} <span className={styles.version}>v{this.state.plugin.version}</span>
+                </h3>
+                <p className={styles.author}>
+                  By{' '}
+                  <a href={this.state.plugin.homepage} target="_blank">
+                    {this.state.plugin.author}
+                  </a>
+                </p>
+                <p>{this.state.plugin.description}</p>
+                <div className={styles.metadataList}>
+                  {/* <div className={styles.metadata}><img className={styles.icon} src={`${this.state.router.basePath}/images/icon-filesize.svg`} alt="Filesize" /> {this.formatBytes(this.state.plugin.size)}</div> */}
+                  <div className={styles.metadata}>
+                    <img
+                      className={styles.icon}
+                      src={`${this.state.router.basePath}/images/icon-date.svg`}
+                      alt="Date updated"
+                    />{' '}
+                    {this.timeSince(this.state.plugin.date)} ago
+                  </div>
+                  <div className={styles.metadata}>
+                    <img className={styles.icon} src={`${this.state.router.basePath}/images/icon-tag.svg`} alt="Tags" />
                     <ul className={styles.tags}>
-                    {this.state.plugin.tags.map((tag: string, tagIndex: number) => (
-                      <li className={styles.tag} key={`${tag}-${tagIndex}`}>{tag},</li>
-                    ))}
-                  </ul>
+                      {this.state.plugin.tags.map((tag: string, tagIndex: number) => (
+                        <li className={styles.tag} key={`${tag}-${tagIndex}`}>
+                          {tag},
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.options}>
-          <div className={styles.row}>
-            <div className={`${styles.cell} ${styles.download}`}>
-              <p>Download and install manually:</p>
-              { this.state.plugin.files.linux ? 
-                <a className={`button ${styles.button}`} href={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.linux.name}`}>Linux</a>
-                : ''
-              }
-              { this.state.plugin.files.mac ?
-                <a className={`button ${styles.button}`} href={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.mac.name}`}>MacOS</a>
-                : ''
-              }
-              { this.state.plugin.files.win ?
-                <a className={`button ${styles.button}`} href={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.win.name}`}>Windows</a>
-                : ''
-              }
-            </div>
-            <div className={`${styles.cell} ${styles.install}`}>
-              <p>Install via command line:</p>
-              <pre className={styles.codeBox}>studiorack install {this.state.plugin.id}</pre>
+          <div className={styles.options}>
+            <div className={styles.row}>
+              <div className={`${styles.cell} ${styles.download}`}>
+                <p>Download and install manually:</p>
+                {this.state.plugin.files.linux ? (
+                  <a
+                    className={`button ${styles.button}`}
+                    href={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.linux.name}`}
+                  >
+                    Linux
+                  </a>
+                ) : (
+                  ''
+                )}
+                {this.state.plugin.files.mac ? (
+                  <a
+                    className={`button ${styles.button}`}
+                    href={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.mac.name}`}
+                  >
+                    MacOS
+                  </a>
+                ) : (
+                  ''
+                )}
+                {this.state.plugin.files.win ? (
+                  <a
+                    className={`button ${styles.button}`}
+                    href={`https://github.com/${this.state.plugin.repo}/releases/download/${this.state.plugin.release}/${this.state.plugin.files.win.name}`}
+                  >
+                    Windows
+                  </a>
+                ) : (
+                  ''
+                )}
+              </div>
+              <div className={`${styles.cell} ${styles.install}`}>
+                <p>Install via command line:</p>
+                <pre className={styles.codeBox}>studiorack install {this.state.plugin.id}</pre>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
-    </Layout>
-    )
+        </article>
+      </Layout>
+    );
   }
 }
-export default withRouter(PluginPage)
+export default withRouter(PluginPage);
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = await pluginsGet()
-  const list = []
+  const paths = await pluginsGet();
+  const list = [];
   for (const pluginId in paths) {
     list.push({
       params: {
-        slug: idToSlug(pluginId)
-      }
-    })
+        slug: idToSlug(pluginId),
+      },
+    });
   }
   return {
     paths: list,
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
 type Params = {
   params: {
-    slug: string
-  }
-}
+    slug: string;
+  };
+};
 
 export async function getStaticProps({ params }: Params) {
-  const pluginId = slugToId(params.slug)
-  const plugin: PluginInterface = await pluginGet(pluginId)
+  const pluginId = slugToId(params.slug);
+  const plugin: PluginInterface = await pluginGet(pluginId);
   console.log(plugin);
   return {
     props: {
-      plugin
-    }
-  }
+      plugin,
+    },
+  };
 }
--- pages/plugins/index.tsx
+++ pages/plugins/index.tsx
@@ -1,58 +1,62 @@
-import { Component, ChangeEvent, SyntheticEvent } from 'react'
-import Head from 'next/head'
-import Layout, { siteTitle } from '../../components/layout'
-import styles from '../../styles/plugins.module.css'
-import Link from 'next/link'
-import { GetStaticProps } from 'next'
-import { withRouter, Router } from 'next/router'
-import { PluginInterface, pluginLatest, pluginsGet } from '@studiorack/core'
-import { idToSlug } from '../../node_modules/@studiorack/core/dist/utils'
+import { Component, ChangeEvent, SyntheticEvent } from 'react';
+import Head from 'next/head';
+import Layout, { siteTitle } from '../../components/layout';
+import styles from '../../styles/plugins.module.css';
+import Link from 'next/link';
+import { GetStaticProps } from 'next';
+import { withRouter, Router } from 'next/router';
+import { PluginInterface, pluginLatest, pluginsGet } from '@studiorack/core';
+import { idToSlug } from '../../node_modules/@studiorack/core/dist/utils';
 
 type PluginListProps = {
-  plugins: PluginInterface[],
-  router: Router
-}
+  plugins: PluginInterface[];
+  router: Router;
+};
 
-class PluginList extends Component<PluginListProps, {
-  pluginsFiltered: PluginInterface[]
-  router: Router
-  query: string,
-}> {
-
+class PluginList extends Component<
+  PluginListProps,
+  {
+    pluginsFiltered: PluginInterface[];
+    router: Router;
+    query: string;
+  }
+> {
   constructor(props: PluginListProps) {
-    super(props)
+    super(props);
     this.state = {
       pluginsFiltered: props.plugins || [],
       router: props.router,
-      query: ''
-    }
+      query: '',
+    };
   }
 
   handleChange = (event: ChangeEvent) => {
-    const el = event.target as HTMLInputElement
-    const query = el.value ? el.value.toLowerCase() : ''
+    const el = event.target as HTMLInputElement;
+    const query = el.value ? el.value.toLowerCase() : '';
     const filtered = this.props.plugins.filter((plugin: PluginInterface) => {
-      if (plugin.name.toLowerCase().indexOf(query) !== -1 ||
+      if (
+        plugin.name.toLowerCase().indexOf(query) !== -1 ||
         plugin.description.toLowerCase().indexOf(query) !== -1 ||
-        plugin.tags.includes(query)) {
-        return plugin
+        plugin.tags.includes(query)
+      ) {
+        return plugin;
       }
-      return false
-    })
+      return false;
+    });
     this.setState({
       pluginsFiltered: filtered || [],
-      query
-    })
-  }
+      query,
+    });
+  };
 
   imageError = (event: SyntheticEvent) => {
-    const el = event.target as HTMLImageElement
-    const fallback = `${this.state.router.basePath}/images/plugin.png`
+    const el = event.target as HTMLImageElement;
+    const fallback = `${this.state.router.basePath}/images/plugin.png`;
     if (el.getAttribute('src') !== fallback) {
-      el.setAttribute('src', fallback)
+      el.setAttribute('src', fallback);
     }
-    return undefined
-  }
+    return undefined;
+  };
 
   render() {
     return (
@@ -62,50 +66,81 @@ class PluginList extends Component<PluginListProps, {
         </Head>
         <section className={styles.plugins}>
           <div className={styles.pluginsHeader}>
-            <h3 className={styles.pluginsTitle}>Plugins <span className={styles.pluginCount}>({this.state.pluginsFiltered.length})</span></h3>
-            <input className={styles.pluginsSearch} placeholder="Filter by keyword" value={this.state.query} onChange={this.handleChange} />
+            <h3 className={styles.pluginsTitle}>
+              Plugins <span className={styles.pluginCount}>({this.state.pluginsFiltered.length})</span>
+            </h3>
+            <input
+              className={styles.pluginsSearch}
+              placeholder="Filter by keyword"
+              value={this.state.query}
+              onChange={this.handleChange}
+            />
           </div>
           <div className={styles.pluginsList}>
             {this.state.pluginsFiltered.map((plugin: PluginInterface, pluginIndex: number) => (
-              <Link href="/plugins/[slug]" as={`/plugins/${idToSlug(plugin.repo + '/' + plugin.id)}`} key={`${idToSlug(plugin.repo + '/' + plugin.id)}-${pluginIndex}`}>
+              <Link
+                href="/plugins/[slug]"
+                as={`/plugins/${idToSlug(plugin.repo + '/' + plugin.id)}`}
+                key={`${idToSlug(plugin.repo + '/' + plugin.id)}-${pluginIndex}`}
+              >
                 <div className={styles.plugin}>
                   <div className={styles.pluginDetails}>
                     <div className={styles.pluginHead}>
-                      <h4 className={styles.pluginTitle}>{plugin.name} <span className={styles.pluginVersion}>v{plugin.version}</span></h4>
-                      <span className={styles.pluginButton}><img className={styles.pluginButtonIcon} src={`${this.state.router.basePath}/images/icon-download.svg`} alt="Download" /></span>
+                      <h4 className={styles.pluginTitle}>
+                        {plugin.name} <span className={styles.pluginVersion}>v{plugin.version}</span>
+                      </h4>
+                      <span className={styles.pluginButton}>
+                        <img
+                          className={styles.pluginButtonIcon}
+                          src={`${this.state.router.basePath}/images/icon-download.svg`}
+                          alt="Download"
+                        />
+                      </span>
                     </div>
                     <ul className={styles.pluginTags}>
-                      <img className={styles.pluginIcon} src={`${this.state.router.basePath}/images/icon-tag.svg`} alt="Tags" />
+                      <img
+                        className={styles.pluginIcon}
+                        src={`${this.state.router.basePath}/images/icon-tag.svg`}
+                        alt="Tags"
+                      />
                       {plugin.tags.map((tag: string, tagIndex: number) => (
-                        <li className={styles.pluginTag} key={`${tag}-${tagIndex}-${pluginIndex}`}>{tag},</li>
+                        <li className={styles.pluginTag} key={`${tag}-${tagIndex}-${pluginIndex}`}>
+                          {tag},
+                        </li>
                       ))}
                     </ul>
                   </div>
-                  { plugin.files.image ?
-                    <img className={styles.pluginImage} src={`https://github.com/${plugin.repo}/releases/download/${plugin.release}/${plugin.files.image.name}`} alt={plugin.name} onError={this.imageError} />
-                    : ""
-                  }
+                  {plugin.files.image ? (
+                    <img
+                      className={styles.pluginImage}
+                      src={`https://github.com/${plugin.repo}/releases/download/${plugin.release}/${plugin.files.image.name}`}
+                      alt={plugin.name}
+                      onError={this.imageError}
+                    />
+                  ) : (
+                    ''
+                  )}
                 </div>
               </Link>
             ))}
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
-export default withRouter(PluginList)
+export default withRouter(PluginList);
 
 export const getStaticProps: GetStaticProps = async () => {
-  const plugins = await pluginsGet()
-  const list:PluginInterface[] = []
+  const plugins = await pluginsGet();
+  const list: PluginInterface[] = [];
   for (const pluginId in plugins) {
-    const plugin: PluginInterface = pluginLatest(plugins[pluginId])
-    list.push(plugin)
+    const plugin: PluginInterface = pluginLatest(plugins[pluginId]);
+    list.push(plugin);
   }
   return {
     props: {
-      plugins: list
-    }
-  }
-}
+      plugins: list,
+    },
+  };
+};
