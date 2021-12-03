61bdb6f4fc062de6107974406c1121729f5d5f76
kmturley <kmturley@users.noreply.github.com>
2021-12-03 05:20:12 +0000

    deploy: 844686559a090fad82ecdf03dbcf090b383d2351

--- _next/static/latest/_ssgManifest.js
+++ _next/static/latest/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdocs","\u002Fplugins","\u002Fdocs\u002F[slug]","\u002Fplugins\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002Fdocs","\u002Fplugins","\u002Fdocs\u002F[slug]","\u002Fplugins\u002F[userId]\u002F[repoId]\u002F[pluginId]","\u002Fplugins\u002F[userId]\u002F[repoId]","\u002Fplugins\u002F[userId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
