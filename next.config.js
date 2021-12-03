9069aa6b9b890bc87d3bb2f07149c8a58bf63761
Kim T <kimturley@gmail.com>
2021-12-02 21:55:07 -0800

    Test/view static site locally

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-// const package = require('./package.json')
+// const package = require('./package.json')'
+
+console.log('NODE_ENV', process.env.NODE_ENV);
 
 module.exports = {
   basePath: (process.env.NODE_ENV === 'production') ? '/studiorack-site' : '',
