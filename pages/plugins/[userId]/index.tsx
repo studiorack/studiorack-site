281ef771b87fdb163f31ac0084667476700ca0ec
Kim T <kimturley@gmail.com>
2021-12-28 19:06:38 -0800

    Use grid component

--- pages/plugins/[userId]/index.tsx
+++ pages/plugins/[userId]/index.tsx
@@ -1,12 +1,11 @@
-import { Component, ChangeEvent, SyntheticEvent } from 'react';
+import { Component, ChangeEvent } from 'react';
 import Head from 'next/head';
 import Crumb from '../../../components/crumb';
 import Layout, { siteTitle } from '../../../components/layout';
 import styles from '../../../styles/plugins.module.css';
-import Link from 'next/link';
+import GridItem from '../../../components/grid-item';
 import { withRouter, Router } from 'next/router';
 import { PluginInterface, pluginLatest, PluginPack, pluginsGet } from '@studiorack/core';
-import { pluginFileUrl } from '../../../node_modules/@studiorack/core/dist/utils';
 
 type PluginListProps = {
   plugins: PluginInterface[];
@@ -52,15 +51,6 @@ class PluginList extends Component<
     });
   };
 
-  imageError = (event: SyntheticEvent) => {
-    const el = event.target as HTMLImageElement;
-    const fallback = `${this.state.router.basePath}/images/plugin.png`;
-    if (el.getAttribute('src') !== fallback) {
-      el.setAttribute('src', fallback);
-    }
-    return undefined;
-  };
-
   render() {
     return (
       <Layout>
@@ -72,53 +62,7 @@ class PluginList extends Component<
           <h2>{this.state.userId}</h2>
           <div className={styles.pluginsList}>
             {this.state.pluginsFiltered.map((plugin: PluginInterface, pluginIndex: number) => (
-              <Link
-                href="/plugins/[userId]/[repoId]/[pluginId]"
-                as={`/plugins/${plugin.repo}/${plugin.id}`}
-                key={`${plugin.repo}/${plugin.id}-${pluginIndex}`}
-              >
-                <div className={styles.plugin}>
-                  <div className={styles.pluginDetails}>
-                    <div className={styles.pluginHead}>
-                      <h4 className={styles.pluginTitle}>
-                        {plugin.name} <span className={styles.pluginVersion}>v{plugin.version}</span>
-                      </h4>
-                      <span className={styles.pluginButton}>
-                        <img
-                          className={styles.pluginButtonIcon}
-                          src={`${this.state.router.basePath}/images/icon-download.svg`}
-                          alt="Download"
-                          loading="lazy"
-                        />
-                      </span>
-                    </div>
-                    <ul className={styles.pluginTags}>
-                      <img
-                        className={styles.pluginIcon}
-                        src={`${this.state.router.basePath}/images/icon-tag.svg`}
-                        alt="Tags"
-                        loading="lazy"
-                      />
-                      {plugin.tags.map((tag: string, tagIndex: number) => (
-                        <li className={styles.pluginTag} key={`${tag}-${tagIndex}-${pluginIndex}`}>
-                          {tag},
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  {plugin.files.image ? (
-                    <img
-                      className={styles.pluginImage}
-                      src={pluginFileUrl(plugin, 'image')}
-                      alt={plugin.name}
-                      onError={this.imageError}
-                      loading="lazy"
-                    />
-                  ) : (
-                    ''
-                  )}
-                </div>
-              </Link>
+              <GridItem plugin={plugin} pluginIndex={pluginIndex}></GridItem>
             ))}
           </div>
         </section>
