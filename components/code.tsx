ce81935149c9f4bcb8d01fce7b6690f65b7f5dc0
Kim T <kimturley@gmail.com>
2024-09-06 16:15:08 +0100

    Fix js error

--- components/code.tsx
+++ components/code.tsx
@@ -12,20 +12,20 @@ const Code = ({ plugin }: CodeProps) => (
       <a href="https://www.npmjs.com/package/@studiorack/cli" target="_blank">
         StudioRack CLI
       </a>
-      {plugin.tags.includes('sfz') ? (
-        <span>
-          <pre className={styles.codeLine}>studiorack plugin install sfztools/sfizz</pre>
-          <pre className={styles.codeLine}>studiorack plugin install {plugin.id}</pre>
-        </span>
-      ) : plugin.tags.includes('sf2') ? (
-        <span>
-          <pre className={styles.codeLine}>studiorack plugin install studiorack/juicysf</pre>
-          <pre className={styles.codeLine}>studiorack plugin install {plugin.id}</pre>
-        </span>
-      ) : (
-        <pre className={styles.codeLine}>studiorack plugin install {plugin.id}</pre>
-      )}
     </p>
+    {plugin.tags.includes('sfz') ? (
+      <span>
+        <pre className={styles.codeLine}>studiorack plugin install sfztools/sfizz</pre>
+        <pre className={styles.codeLine}>studiorack plugin install {plugin.id}</pre>
+      </span>
+    ) : plugin.tags.includes('sf2') ? (
+      <span>
+        <pre className={styles.codeLine}>studiorack plugin install studiorack/juicysf</pre>
+        <pre className={styles.codeLine}>studiorack plugin install {plugin.id}</pre>
+      </span>
+    ) : (
+      <pre className={styles.codeLine}>studiorack plugin install {plugin.id}</pre>
+    )}
   </div>
 );
 
