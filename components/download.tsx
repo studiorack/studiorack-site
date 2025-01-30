5515a33a25e2585fcc0de26249b3ad0c4ff96299
Kim T <kimturley@gmail.com>
2025-01-29 22:14:26 -0800

    Download file buttons with format and combine multi-platform

--- components/download.tsx
+++ components/download.tsx
@@ -9,42 +9,51 @@ type DownloadsProps = {
 const Downloads = ({ plugin }: DownloadsProps) => (
   <div className={`${styles.download}`}>
     <p>Download and install manually:</p>
-    {/* {plugin.tags.includes('sfz') || plugin.tags.includes('sf2') ? (
+    {plugin.tags.includes('sfz') || plugin.tags.includes('sf2') ? (
       <span>
-        <a className={`button ${styles.downloadButton}`} href={plugin.files[0].url} title="High-quality">
-          High-quality
-          <img
-            className={styles.downloadButtonIcon}
-            src={`${getBasePath()}/images/icon-download.svg`}
-            alt="Download"
-            loading="lazy"
-          />
-        </a>
-      </span>
-    ) : ( */}
-    <span>
-      {plugin.files.map((file: PluginFile | PresetFile | ProjectFile) => (
-        <div className={styles.downloadFile}>
-          {file.systems.map((system: System) => (
-            <div className={`${styles.downloadSystem} ${styles['icon-' + system.type]}`}>
-              <span className={styles.downloadSystemName}>
-                {systemTypes.filter(systemType => systemType.value === system.type)[0].name}
-              </span>
+        {plugin.files.map((file: PluginFile | PresetFile | ProjectFile) => (
+          <div className={styles.downloadFile}>
+            <div className={`${styles.downloadSystem} ${styles.iconAll}`}>
+              <span className={styles.downloadSystemName}>Multi-platform</span>
               <span className={styles.downloadSystemArch}>{file.architectures.join(', ')}</span>
             </div>
-          ))}
-          <a className={`button ${styles.downloadButton}`} href={file.url} title="Download">
-            <img
-              className={styles.downloadButtonIcon}
-              src={`${getBasePath()}/images/icon-download.svg`}
-              alt="Download"
-              loading="lazy"
-            />
-          </a>
-        </div>
-      ))}
-    </span>
-    {/* )} */}
+            <a className={`button ${styles.downloadButton}`} href={file.url} title="Download">
+              .{file.format}
+              <img
+                className={styles.downloadButtonIcon}
+                src={`${getBasePath()}/images/icon-download.svg`}
+                alt="Download"
+                loading="lazy"
+              />
+            </a>
+          </div>
+        ))}
+      </span>
+    ) : (
+      <span>
+        {plugin.files.map((file: PluginFile | PresetFile | ProjectFile) => (
+          <div className={styles.downloadFile}>
+            {file.systems.map((system: System) => (
+              <div className={`${styles.downloadSystem} ${styles['icon-' + system.type]}`}>
+                <span className={styles.downloadSystemName}>
+                  {systemTypes.filter(systemType => systemType.value === system.type)[0].name}
+                </span>
+                <span className={styles.downloadSystemArch}>{file.architectures.join(', ')}</span>
+              </div>
+            ))}
+            <a className={`button ${styles.downloadButton}`} href={file.url} title="Download">
+              .{file.format}
+              <img
+                className={styles.downloadButtonIcon}
+                src={`${getBasePath()}/images/icon-download.svg`}
+                alt="Download"
+                loading="lazy"
+              />
+            </a>
+          </div>
+        ))}
+      </span>
+    )}
   </div>
 );
 
